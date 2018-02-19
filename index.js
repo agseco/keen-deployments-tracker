b622591fb75f539797a5fe6e303663bac4a93b15
Alejandro Garcia Seco <ags@caravelo.com>
2018-02-19 14:39:44 +0100

    Default error handling

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 
 const DeploymentsTracker = require('./lib/deployments-tracker.js');
-new DeploymentsTracker().run();
+const dt = new DeploymentsTracker();
+
+const run = async function () {
+  try {
+    await dt.run();
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
+
+run();
